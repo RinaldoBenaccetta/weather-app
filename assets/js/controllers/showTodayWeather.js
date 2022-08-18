eac360f7f8fa78032f694c298fc461230792941d
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-08-18 12:33:58 +0200

    fix hour

--- assets/js/controllers/showTodayWeather.js
+++ assets/js/controllers/showTodayWeather.js
@@ -4,6 +4,8 @@ import { mainCard } from "../views/mainCard.js";
 export const showTodayWeather = async (lat, lon) => {
     const todayWeather = await getTodayWeather(lat, lon)
 
+    // console.log(todayWeather);
+
     // test with first
     mainCard(todayWeather[0], '01')
 }
--- assets/js/models/getTodayWeather.js
+++ assets/js/models/getTodayWeather.js
@@ -13,9 +13,14 @@ const parseResponse = (response) => {
     // each object represent 3 hours, so 8 * 3 = 24 hours
     const todayArray = response.list.slice(0, 8)
 
+    const city = response.city
+
+    console.log(todayArray)
+    console.log(city);
+
     const formatedArray = todayArray.map((day) => {
         return {
-            hour: getHour(day.dt),
+            hour: getHour((day.dt - city.timezone) * 1000),// provided timestamp is in unix
             icon: '',
             state: day.weather[0].description,
             temp: getTemp(day.main.temp),
