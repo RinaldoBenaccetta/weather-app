460ce6c7a893db4c89b74cc59f4263f606b85b0a
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-08-18 09:50:57 +0200

    Add application data files.

---
+++ assets/js/model/application.js
@@ -0,0 +1,7 @@
+export const APPLICATION = {
+    element: {
+        //imageContainer: document.querySelector('#image'),
+    },
+    //request: 'https://thatsthespir.it/api',
+    //defaultImage: '../assets/img/avatar.jpg'
+}
