fbbe1f1710c08284feab4f8d279676a5d9b0a915
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-08-23 09:14:55 +0200

    wrap in iife

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,10 @@
-// import { apiQuery } from "./models/apiQuery.js"
-// import { weatherController } from "./controllers/weatherController.js"
-
-// import { encodeInput } from "./utils/encodeInput.js";
-
-// import { getAutocomplete } from "./models/getAutocomplete.js";
-
 import { cityInputListener } from "./controllers/cityInputListener.js";
-
 import { searchButtonListener } from "./controllers/searchButtonListener.js"
 
-// try bxl query
-// console.log(apiQuery(50.85034, 4.35171))
-
-// weatherController(50.85034, 4.35171)
-// weatherController('bruxelles')
-
-searchButtonListener()
-
-cityInputListener()
-
-//console.log(getAutocomplete("bru"));
-// console.log(encodeInput("<div>hello</div>"));
-
-// const test = 'https://api.teleport.org/api/cities/?search=Bruxelles'
-
-// const testA = async () => {
-//     const response = await fetch(test)
-//     const jsonResponse = await response.json()
-//     console.log(jsonResponse);
-// }
-
-// testA()
+/**
+ * Add listener to search button and city input.
+ */
+(() => {
+    searchButtonListener()
+    cityInputListener()
+})()
