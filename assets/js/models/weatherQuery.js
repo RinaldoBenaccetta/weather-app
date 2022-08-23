ce4795f2aecb212c74fef481aa04b053a2bfcf97
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-08-23 10:12:44 +0200

    try to catch API error response not solved.

--- assets/js/models/weatherQuery.js
+++ assets/js/models/weatherQuery.js
@@ -9,14 +9,40 @@ import { cityWeatherRequest } from "./request.js"
  * @returns {Object|False} 
  */
 export const weatherQuery = async (city) => {
-    try {
-        const response = await fetch(cityWeatherRequest(city))
-        const jsonResponse = await response.json()
+    const response = await fetchCityWeather(city)
 
-        return jsonResponse.cod === '200' ? jsonResponse : false
+    console.log(response)
+
+    return response ? await response.json() : false
+    // const jsonResponse = await response.json()
+
+    // return jsonResponse.cod === '200' ? jsonResponse : false
+
+
+    // try {
+    //     const response = await fetch(cityWeatherRequest(city))
+    //     const jsonResponse = await response.json()
+
+    //     return jsonResponse.cod === '200' ? jsonResponse : false
+
+    // } catch (error) {
+    //     console.log('There is an error :', error)
+    //     return false
+    // }
+}
+
+const fetchCityWeather = async (city) => {
+    try {
+        const jsonResponse = await fetch(cityWeatherRequest(city))
+        console.log(jsonResponse)
+        console.log(jsonResponse.status);
+
+        return jsonResponse.status === 200 ? jsonResponse : false
 
     } catch (error) {
-        console.log(error);
+        console.log('There is an error :', error)
         return false
     }
 }
+
+// https://stackoverflow.com/questions/51781137/how-can-i-handle-error-404-in-async-await-fetch-api
