b02719bd98a203fcd42aad418b3212765928f564
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-09-18 15:04:24 +0200

    Add getElement function that use querySelector in order to maximize minification.

--- src/assets/js/models/elements.js
+++ src/assets/js/models/elements.js
@@ -1,20 +1,30 @@
 import { APPLICATION } from "./application.js"
 
 /**
+ * Return the DOM element corresponding to provided selector.
+ *
+ * @param id
+ * @returns {Element}
+ */
+const getElement = (id) => {
+    return document.querySelector(id)
+}
+
+/**
  * An object with elements of the DOM.
  *
  * @type {{mainContainer: Element, footerContainer: Element, autocompleteList: Element, todayCardsContainer: Element, weatherTitleContainer: Element, searchButton: Element, citySearchInput: Element, headerContainer: Element, autocompleteContainer: Element, main: Element, body: HTMLElement}}
  */
 export const ELEMENTS = {
     body: document.body,
-    main: document.querySelector(APPLICATION.id.main.container),
-    headerContainer: document.querySelector(APPLICATION.id.header.container),
-    mainContainer: document.querySelector(APPLICATION.id.main.container),
-    footerContainer: document.querySelector(APPLICATION.id.footer.container),
-    autocompleteList: document.querySelector(APPLICATION.id.header.searchContainer.autocomplete.list),
-    autocompleteContainer: document.querySelector(APPLICATION.id.header.searchContainer.autocomplete.container),
-    todayCardsContainer: document.querySelector(APPLICATION.id.main.cards.container),
-    weatherTitleContainer: document.querySelector(APPLICATION.id.main.localisation),
-    citySearchInput: document.querySelector(APPLICATION.id.header.searchContainer.townInput),
-    searchButton: document.querySelector(APPLICATION.id.header.searchContainer.search),
+    main: getElement(APPLICATION.id.main.container),
+    headerContainer: getElement(APPLICATION.id.header.container),
+    mainContainer: getElement(APPLICATION.id.main.container),
+    footerContainer: getElement(APPLICATION.id.footer.container),
+    autocompleteList: getElement(APPLICATION.id.header.searchContainer.autocomplete.list),
+    autocompleteContainer: getElement(APPLICATION.id.header.searchContainer.autocomplete.container),
+    todayCardsContainer: getElement(APPLICATION.id.main.cards.container),
+    weatherTitleContainer: getElement(APPLICATION.id.main.localisation),
+    citySearchInput: getElement(APPLICATION.id.header.searchContainer.townInput),
+    searchButton: getElement(APPLICATION.id.header.searchContainer.search),
 }
