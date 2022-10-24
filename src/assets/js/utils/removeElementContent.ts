2aabceeb184ec5e53cd5bfa7064ab39bb97946be
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-10-24 14:19:30 +0200

    Add doc to removeAutocompleteContent.ts

--- src/assets/js/utils/removeElementContent.ts
+++ src/assets/js/utils/removeElementContent.ts
@@ -1,3 +1,8 @@
+/**
+ * Remove the inner HTML of the provided container if it exists.
+ *
+ * @param container
+ */
 export const removeElementContent = (container: Element | undefined): void => {
     if (container) container.innerHTML = ''
 }
