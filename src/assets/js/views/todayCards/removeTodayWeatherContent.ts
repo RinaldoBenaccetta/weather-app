ce6c2714369db677b3f4bbb3ff1fced4aabff682
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-10-24 14:09:23 +0200

    remove unused removeTodayWeatherContent.ts

--- src/assets/js/views/todayCards/removeTodayWeatherContent.ts
+++
@@ -1,8 +0,0 @@
-import { ELEMENTS } from "../../models/elements.js"
-
-/**
- * Remove the content of the cards' container.
- */
-export const removeTodayWeatherContent = (): void => {
-    ELEMENTS.todayCardsContainer.innerHTML = ''
-}
